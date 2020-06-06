aab8c9aed7837a817c6d5655d77dfa804d20185d
aoi <s19006@std.it-college.ac.jp>
2020-06-06 15:49:56 +0900

    a

--- python3/kadai/test04.py
+++ python3/kadai/test04.py
@@ -1,7 +1,12 @@
 a = "Hi He Lied Because Boron Cloud Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause Arthur King Can."
+
+y = [1, 5, 6, 7, 8, 9, 15, 16, 19]
 b = a.split(" ")
-c = [(b[idx - 1][0], idx) for idx in [1, 5, 6, 7, 8, 9, 15, 16, 19]]
-d = [(b[idx - 1][:2], idx) for idx in [2, 3, 4, 10, 11, 12, 13, 14, 17, 18, 20]]
-cd = {x: y for (x, y) in c + d}
-print(cd)
+c = {}
+for d, word in enumerate(b):
+    if d + 1 in y:
+        c[d + 1] = word[0]
+    else:
+        c[d + 1] = word[:2]
+print(c)
 
